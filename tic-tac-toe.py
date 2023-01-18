173e6dc4b2ae7b0aa062e6e187c3a13840354049
Charles Nganga <ngangacharles44@gmail.com>
2023-01-18 18:49:24 +0300

    added tic-tac-toe game where I begin error handling

---
+++ tic-tac-toe.py
@@ -0,0 +1,33 @@
+game = [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]]
+
+
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
+    try:
+        if not just_display:
+            game_map[row][column] = player
+        print("   a  b  c")
+        for count, row in enumerate(game_map):
+            print(count, row)
+        return game_map
+    except IndexError as e:
+        print("Error: Make sure you input row/column  as 0, 1 or 2", e)
+
+
+game = game_board(game, just_display=True)
+
+game = game_board(game, 1, 3, 2)
+
+
+
+# slice and index
+# numbers = [1,2,3,4,5]
+# # index
+# print(numbers[1])
+# # last index
+# print(numbers[-1])
+# # slice
+# print(numbers[0:3])
+# # slice from to last
+# print(numbers[3:])
