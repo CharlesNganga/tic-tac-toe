42177f8209a3ab3ebde09d2fe76337d7dcef7c70
Charles Nganga <ngangacharles44@gmail.com>
2023-01-18 23:42:02 +0300

    did basic error handling

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -13,11 +13,14 @@ def game_board(game_map, player=0, row=0, column=0, just_display=False):
         return game_map
     except IndexError as e:
         print("Error: Make sure you input row/column  as 0, 1 or 2", e)
+    except Exception as e:
+        print("Something went very wrong!!", e)
+
 
 
 game = game_board(game, just_display=True)
 
-game = game_board(game, 1, 3, 2)
+game = game_board(game, 1, 2, 2)
 
 
 
