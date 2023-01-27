616fed0f38074b212df20c4d8b3d0244652570da
Charles Nganga <ngangacharles44@gmail.com>
2023-01-27 11:47:26 +0300

    removed the unnecessary comments at the bottom

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,13 +1,13 @@
-game = [[0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]]
+game = [[2, 0, 2],
+        [1, 1, 1],
+        [1, 0, 2]]
 
 
 def game_board(game_map, player=0, row=0, column=0, just_display=False):
     try:
         if not just_display:
             game_map[row][column] = player
-        print("   a  b  c")
+        print("   0  1  2")
         for count, row in enumerate(game_map):
             print(count, row)
         return game_map
@@ -17,20 +17,6 @@ def game_board(game_map, player=0, row=0, column=0, just_display=False):
         print("Something went very wrong!!", e)
 
 
-
 game = game_board(game, just_display=True)
 
 game = game_board(game, 1, 2, 2)
-
-
-
-# slice and index
-# numbers = [1,2,3,4,5]
-# # index
-# print(numbers[1])
-# # last index
-# print(numbers[-1])
-# # slice
-# print(numbers[0:3])
-# # slice from to last
-# print(numbers[3:])
