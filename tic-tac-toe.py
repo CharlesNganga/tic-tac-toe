a7485da3c42f204fa60865310bf80a38a93bb12a
Charles Nganga <ngangacharles44@gmail.com>
2023-01-31 14:32:09 +0300

    made the 0, 1, 2 for playes to be space, colors using colorama library

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,4 +1,6 @@
 import itertools
+from colorama import Fore, Back, Style, init
+init()
 
 
 def win(current_game):
@@ -50,7 +52,15 @@ def game_board(game_map, player=0, row=0, column=0, just_display=False):
             game_map[row][column] = player
         print("   " + "  ".join([str(i) for i in range(len(game_map))]))
         for count, row in enumerate(game_map):
-            print(count, row)
+            colored_row = ""
+            for item in row:
+                if item == 0:
+                    colored_row += "   "
+                elif item == 1:
+                    colored_row += Fore.GREEN + " X " + Style.RESET_ALL
+                elif item == 2:
+                    colored_row += Fore.MAGENTA + " O " + Style.RESET_ALL
+            print(count, colored_row)
         return game_map, True
     except IndexError as e:
         print("Error: Make sure you input row/column  as 0, 1 or 2", e)
@@ -63,9 +73,8 @@ def game_board(game_map, player=0, row=0, column=0, just_display=False):
 play = True
 players = [1, 2]
 while play:
-    game = [[0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]]
+    game_size = int(input("What size game of tic tac toe? "))
+    game = [[0 for i in range(game_size)] for i in range(game_size)]
     game_won = False
     game, _ = game_board(game, just_display=True)  # _ means we don't care about the return
     player_choice = itertools.cycle([1, 2])
